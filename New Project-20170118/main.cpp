1f43d72c7443be5b52bf6cbf3a0c63a692f26337
marizzzzzzzzzzzzzzzzzzzzzzz69 <maris.janens@gmail.com>
2017-01-18 13:42:39 +0200

    Created by Coding Ground Client

---
+++ New Project-20170118/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int x;
+int y;
+
+int summa;
+int reizinajums;
+int dalijums;
+int atnemsana;
+
+summa = x + y;
+reizinajums = x * y;
+dalijums = x / y;
+atnemsana=x - y;
+
+x = 5;
+ y = 10;
+ 
+summa = x + y;
+
+cout << " Skaitla " << x << " un " << y << " summa ir " << summa << endl;
+
+
+
+
+    cout << "ah uakbar is life" << endl;
+}
+
